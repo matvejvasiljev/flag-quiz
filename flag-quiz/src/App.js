9eaf8d68d7a26fab3f8df6222329d14c0ff2a5ee
matvejvasiljev <matvej.vasiljev.2010@icloud.com>
2023-05-29 15:56:29 +0300

    Random buttons, score, lives

--- flag-quiz/src/App.js
+++ flag-quiz/src/App.js
@@ -5,17 +5,52 @@ import data from "./countries.json";
 
 export default function Game() {
   const [currentFlag, setFlag] = useState(Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))])
-  const [buttons, setButtons] = useState([data[currentFlag], data[Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))]], data[Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))]], data[Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))]]])
+  let randButtons = [data[currentFlag], data[Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))]], data[Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))]], data[Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))]]]
+  for (let i = 3; i > 0; i--) {
+    let random = Math.floor(Math.random() * (i + 1))
+    let slot = randButtons[i]
+    randButtons[i] = randButtons[random]
+    randButtons[random] = slot
+  }
+  const [buttons, setButtons] = useState(randButtons)
+  const [score, setScore] = useState(0)
+  const [lives, setLives] = useState(3)
 
-    return (
-        <div className="game">
-          <form action="">
-            <h1>Flag Quiz</h1>
-            <img src={"flags/" + currentFlag.toLowerCase() + ".svg"} alt="" />
-            {buttons.map((element, id) => (
-              <button key={id}>{element}</button>
-            ))}
-          </form>
-        </div>
-    )
+  function restart() {
+    setFlag(Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))])
+    let randButtons = [data[currentFlag], data[Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))]], data[Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))]], data[Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))]]]
+    console.log(data[currentFlag]);
+    for (let i = 3; i > 0; i--) {
+      let random = Math.floor(Math.random() * (i + 1))
+      let slot = randButtons[i]
+      randButtons[i] = randButtons[random]
+      randButtons[random] = slot
+    }
+    setButtons(randButtons)
+  }
+
+  function answer(element) {
+    if (data[currentFlag] === element) {
+      console.log("Right answer");
+      setScore(score + 1)
+    }
+    else {
+      setLives(lives - 1)
+    }
+    restart()
+  }
+
+  return (
+    <div className="game">
+      <form action="">
+        <h2 className="score">Score: {score}</h2>
+        <h1>Flag Quiz</h1>
+        <h2 className="lives">Lives: {lives}</h2>
+        <img src={"flags/" + currentFlag.toLowerCase() + ".svg"} alt="" />
+        {buttons.map((element, id) => (
+          <button key={id} type="button" onClick={() => answer(element)}>{element}</button>
+        ))}
+      </form>
+    </div>
+  )
 }
