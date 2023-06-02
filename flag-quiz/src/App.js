e6b1e3f5d068b227f62baf6cefdb87a816faa41f
matvejvasiljev <matvej.vasiljev.2010@icloud.com>
2023-06-02 15:16:47 +0300

    Code Refactoring

--- flag-quiz/src/App.js
+++ flag-quiz/src/App.js
@@ -4,8 +4,7 @@ import data from "./countries.json";
 // console.log(Object.keys(data));
 
 export default function App() {
-  const [codes, setCodes] = useState(Object.keys[data])
-  console.log(Object.keys[data])
+  const [codes, setCodes] = useState(Object.keys(data))
   const [currentFlag, setFlag] = useState("")
   const [buttons, setButtons] = useState([])
   const [score, setScore] = useState(0)
@@ -13,30 +12,21 @@ export default function App() {
 
   function getQuiz() {
     let newFlag = codes[Math.floor(Math.random() * (codes.length - 1))]
-    let newButtons = [
-      data[newFlag],
-      data[codes[Math.floor(Math.random() * (codes.length - 1))]],
-      data[codes[Math.floor(Math.random() * (codes.length - 1))]],
-      data[codes[Math.floor(Math.random() * (codes.length - 1))]]
-    ]
-    for (let i = 3; i > 0; i--) {
-      let random = Math.floor(Math.random() * (i + 1))
-      let slot = newButtons[i]
-      newButtons[i] = newButtons[random]
-      newButtons[random] = slot
+    let newButtons = [data[codes[Math.floor(Math.random() * (codes.length - 1))]],]
+    while (newButtons.length < 3) {
+      let newButton = data[codes[Math.floor(Math.random() * (codes.length - 1))]]
+      if (!newButtons.includes(newButton) && newButton !== data[newFlag]) {
+        newButtons.push(newButton)
+      }
     }
+    newButtons.splice(Math.floor(Math.random() * 4), 0, data[newFlag])
     setFlag(newFlag)
     setButtons(newButtons)
-  }
-
-  function restart() {
-    setFlag(Object.keys(data)[Math.floor(Math.random() * (Object.keys(data).length - 1))])
   }
 
   useEffect(() => {
     getQuiz()
   }, [])
-
 
   function answer(element) {
     if (data[currentFlag] === element) {
@@ -46,7 +36,7 @@ export default function App() {
     else {
       setLives(lives - 1)
     }
-    restart()
+    getQuiz()
   }
 
   return (
